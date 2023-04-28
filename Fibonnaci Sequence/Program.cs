070f08fd7f46e3a5845a609bb2e58af8543e2320
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-04-28 16:45:50 -0500

    Fibonacci sequence in C#, Recursive and Iterative

--- Fibonnaci Sequence/Program.cs
+++ Fibonnaci Sequence/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace Fibonnaci_Sequence
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static int fib(int n)
         {
-            Console.WriteLine("Hello, World!");
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            return fib(n - 1) + fib(n - 2);
+        }
+
+        static int fib2(int n)
+        {
+            if(n == 0 || n == 1)
+            {
+                return n;
+            }
+            int slast = 0;
+            int last = 1;
+            int curPos = 2;
+
+            while (curPos <= n) {
+                int temp = last;
+                last = last + slast;
+                slast = temp;
+                curPos++;
+            }
+            return last;
+        }
+        public static void Main(String[] args)
+        {
+            System.Diagnostics.Debug.WriteLine(fib(11));
+            System.Diagnostics.Debug.WriteLine(fib2(11));
         }
     }
 }
